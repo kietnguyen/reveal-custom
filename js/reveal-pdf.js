0edeaedb42ec6f48c999fb5ac485595b1b42a43a
Kiet Nguyen <kietnguyen86@gmail.com>
2015-01-21 07:32:17 -0800

    Fix URL and added CSS for table

--- js/reveal-pdf.js
+++ js/reveal-pdf.js
@@ -8,6 +8,6 @@ if (window.location.search.match(/print-pdf/gi)) {
   var pdfLink = document.createElement('link');
   pdfLink.rel = 'stylesheet';
   pdfLink.type = 'text/css';
-  pdfLink.href = '../../css/pdf-custom.css';
+  pdfLink.href = '../../lib/reveal-custom/css/pdf-custom.css';
   document.getElementsByTagName('head')[0].appendChild(pdfLink);
 }
